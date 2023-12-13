40e4934b7900db748253323819bf89d055aa7d2a
Cameron Beebe <cameron.beebe87@yahoo.com>
2023-12-13 12:43:29 -0600

    please work

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,14 @@
 package org.launchcode;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        String please = "hello im gonna cry";
+
+        System.out.println(Arrays.toString(please.split("o")));
     }
 }
---
+++ control-flow-and-collections/studio/counter-characters/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharacterCounter {
+
+
+    public static void main(String[] args) {
+
+    //figure out how to get user input through terminal
+
+
+        HashMap<String, Int>
+
+
+
+    }
+}
